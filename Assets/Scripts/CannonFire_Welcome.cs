1ed97656faf1696ad6147d38f2f968e63b1303b8
saukaron <saukaron@umich.edu>
2016-11-19 12:56:38 -0500

    scene_ui

--- Assets/Scripts/CannonFire_Welcome.cs
+++ Assets/Scripts/CannonFire_Welcome.cs
@@ -6,12 +6,13 @@ public class CannonFire_Welcome : MonoBehaviour {
     // private int mode = 1;
     // private float rotateIncrement = 50f;
     public int barrel_length = 2;
-    public bool active;
+    // public bool active = true;
     public GameObject projectile_type;
     // public GameObject level;
-    public float power = 20;
+    public float power = 70f;
     public float angle = 0f;
     public GameObject cannon;
+    public float x = 7, y = 7, z = 0;
 
     
 
@@ -22,19 +23,21 @@ public class CannonFire_Welcome : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.Space) && active)
+        if (Input.GetKey(KeyCode.Space))
         {
             Vector3 barrel_vector = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle), 0);
             barrel_vector.Normalize();
 
             GameObject projectile = Instantiate(projectile_type);
             projectile.SetActive(true);
+            projectile.transform.localScale = new Vector3(x, y, z);
+            projectile.transform.localPosition = new Vector3(cannon.transform.localPosition.x, cannon.transform.localPosition.y, cannon.transform.localPosition.z);
 
             Rigidbody projectile_physics = projectile.GetComponent<Rigidbody>();
             projectile.transform.position = transform.position + (barrel_length * barrel_vector);
             projectile_physics.velocity = barrel_vector * power;
 
-            active = false;
+            // active = false;
 
             // level.GetComponent<Level_Script>().New_Location(projectile);
 
