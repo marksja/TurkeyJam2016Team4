f2b5e6d57045d35fb011efd996961000bd3bf556
Yuhan Liu <yuhanl@umich.edu>
2016-11-19 12:32:15 -0500

    new level with mouse

--- Assets/Scripts/CannonRotation.cs
+++ Assets/Scripts/CannonRotation.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CannonRotation : MonoBehaviour
 {
-    private int mode = 1;
+    private int mode = 0;
     private float rotateIncrement = 50f;
     private float powerIncrement = 10f;
     public int barrel_length = 2;
@@ -45,7 +45,45 @@ public class CannonRotation : MonoBehaviour
 
             angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+            if (Input.GetKeyDown(KeyCode.UpArrow) && active)
+            {
+                if (power < 30)
+                {
+                    power += 1f;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow) && active)
+            {
+                if (power > 5)
+                {
+                    power -= 1f;
+                }
+            }
+
+            if (Input.GetKey(KeyCode.Space) && active)
+            {
+                Vector3 barrel_vector = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle), 0);
+                barrel_vector.Normalize();
+
+                GameObject projectile = Instantiate(projectile_type);
+                projectile.SetActive(true);
+
+                Rigidbody projectile_physics = projectile.GetComponent<Rigidbody>();
+                projectile.transform.position = transform.position + (barrel_length * barrel_vector);
+                projectile_physics.velocity = barrel_vector * power;
+
+                active = false;
+
+                level.GetComponent<Level_Script>().New_Location(projectile);
+
+                //AudioSource.PlayClipAtPoint(cannonshotSound, transform.position);
+            }
         }
+
+
+
         if (mode == 1 && active)
         {
             if (Input.GetKey(KeyCode.LeftArrow))
