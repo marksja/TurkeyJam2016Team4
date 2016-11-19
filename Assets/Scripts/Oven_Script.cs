3ae02b4c4dd75aee9d2c1e1ae91528337919c4f5
Jacob <marksja@umich.edu>
2016-11-19 11:54:30 -0500

    Such Organized, Very Hierarchy, Wow

---
+++ Assets/Scripts/Oven_Script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Oven_Script : MonoBehaviour {
+
+	public int num_cannons;
+
+	public float time;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+	}
+
+	void OnCollisionEnter(Collision collision){
+		GameObject level = GameObject.Find("Level");
+		level.GetComponent<Level_Script>().New_Location(gameObject);
+		Destroy(collision.collider.gameObject); 
+		//Go to victory screen
+		//Calculate score or whatever
+	}
+}
