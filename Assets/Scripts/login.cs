6be71fa7bd511f2b6711fc381cf2b44e2a3514f2
Yuhan Liu <yuhanl@umich.edu>
2016-11-19 14:51:16 -0500

    update audio for scence ui

---
+++ Assets/Scripts/login.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class login : MonoBehaviour {
+
+    public AudioClip TurkeyTreeSound;
+    public float time;
+    public int twelve = 12;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("PlayAudio", 0.0f, 12.0f);
+    }
+	
+    void PlayAudio()
+    {
+        AudioSource.PlayClipAtPoint(TurkeyTreeSound, transform.position);
+    }
+
+	// Update is called once per frame
+	void Update () {
+    }
+}
