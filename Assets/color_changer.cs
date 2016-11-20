4cc7ab59cf038f3afa169c23db885d93405a9b5c
Jacob <marksja@umich.edu>
2016-11-20 10:56:37 -0500

    added win screen stuff

---
+++ Assets/color_changer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class color_changer : MonoBehaviour {
+
+	public Color next_color;
+	public Color in_transition;
+	public Color previous_color;
+	private Text t;
+
+	// Use this for initialization
+	void Start () {
+		t = GetComponent<Text>();
+		in_transition = Color.red;
+		previous_color = Color.red;
+		next_color = Color.blue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		in_transition = Color.Lerp(previous_color, next_color, Mathf.PingPong(Time.time, 1));
+		t.color = in_transition;
+	}
+}
